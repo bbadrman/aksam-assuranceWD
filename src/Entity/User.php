78b03ee458d76708a50b3212ea2997d655a585d5
bbadrman <bbadrman@gmail.com>
2023-06-14 19:23:00 +0100

    mis ajour ENtity User*

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -170,7 +170,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     {
         $roles = $this->roles;
         // guarantee every user at least has ROLE_USER
-        $roles[] = 'ROLE_SUPER_ADMIN';
+        $roles[] = 'ROLE_USER';
 
         return array_unique($roles);
     }
